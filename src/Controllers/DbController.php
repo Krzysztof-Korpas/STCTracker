b93309aefd95078d15a188968b781901cdb8c702
Krzysztof <nxkorpas@gmail.com>
2021-02-15 17:31:36 +0100

    Add console controller

--- src/Controllers/DbController.php
+++ src/Controllers/DbController.php
@@ -2,6 +2,8 @@
 
 namespace STCoresTracker\Controllers;
 
+use PHPUnit\Framework\Constraint\Count;
+
 class DbController
 {
 
@@ -15,14 +17,14 @@ class DbController
         $this->db = new \PDO($dsn, $User, $Pass);
     }
 
-    public function Select(string $what = null, string $from, string $whare = "1=1")
+    public function Select(string $what = null, string $Table, string $whare = "1=1")
     {
 
         $what = htmlspecialchars($what, ENT_QUOTES);
         $what = $what ?? "*";
         $whare = $whare ?? '1';
 
-        $sql = "SELECT $what FROM $from WHERE ?";
+        $sql = "SELECT $what FROM $Table WHERE ?";
         $query = $this->db->prepare($sql);
         $query->execute([$whare]);
         $response = $query->fetchAll(\PDO::FETCH_ASSOC);
@@ -30,12 +32,12 @@ class DbController
         return $response;
     }
 
-    public function Insert(string $InTo, array $Data)
+    public function Insert(string $Table, array $Data)
     {
         $TempArrayOfColumns = [];
         $sub1 = "";
         $sub2 = "";
-        $TempSql = "DESCRIBE $InTo";
+        $TempSql = "DESCRIBE $Table";
         $query = $this->db->prepare($TempSql);
         $query->execute();
         $response = $query->fetchAll(\PDO::FETCH_ASSOC);
@@ -63,20 +65,51 @@ class DbController
             }
         }
 
-        $sql = "INSERT INTO $InTo ($sub1) VALUES ($sub2)";
+        $sql = "INSERT INTO $Table ($sub1) VALUES ($sub2)";
 
          $query = $this->db->prepare($sql);
         if(!$query->execute()){
-            echo "Insert not working \n";
-            print_r($query->errorInfo());
+          
             return false;
         }else{
            
 
             return true;
         }
-        
-        //echo $sql;
+      
+    }
+
+    public function Update(string $Table, array $Data,array $whare){
+
+        $sql = "UPDATE `$Table` SET ";
+
+        for($i=0;$i<count($Data);$i++){
+            if($i>0){
+                $sql = $sql.",";
+            }
+            $sql = $sql . "`".array_keys($Data)[$i]."` = '".$Data[array_keys($Data)[$i]]."'";
+        }
+        for($i=0;$i<count($whare);$i++){
+            if($i==0){
+                $sql = $sql." WHERE `".array_keys($whare)[$i]."`".$whare[array_keys($whare)[$i]][1]." ".$whare[array_keys($whare)[$i]][0];
+            }else{
+                if($i>0){
+                    $sql = $sql.",";
+                }
+                $sql = $sql. $whare[array_keys($whare)[$i]][2] ." `".array_keys($whare)[$i]."` ".$whare[array_keys($whare)[$i]][1]." '".$whare[array_keys($whare)[$i]][0]."'";
+            }
+        }
+    
+     $query = $this->db->prepare($sql);
+        if(!$query->execute()){
+            
+            
+            return false;
+        }else{
+           
+
+            return true;
+        }
     }
 
     public function DbReturn()
